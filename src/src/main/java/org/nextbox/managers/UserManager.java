06470651fa4bd64e60eaf5ca0f91702878d0ad68
Saurabh Sood <saurabhsood91@gmail.com>
2017-04-19 23:05:06 -0600

    save home dir

--- src/src/main/java/org/nextbox/managers/UserManager.java
+++ src/src/main/java/org/nextbox/managers/UserManager.java
@@ -2,9 +2,15 @@ package org.nextbox.managers;
 
 import org.nextbox.beans.NextboxProperties;
 import org.nextbox.model.User;
+import org.nextbox.service.FilesystemAPI;
+import org.nextbox.service.FilesystemService;
 import org.nextbox.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by saurabh on 3/21/17.
@@ -22,11 +28,18 @@ public class UserManager {
         return userService.getUserByUsername(username);
     }
 
-    public boolean createAccount(User user) {
+    public boolean createAccount(User user) throws FileNotFoundException {
 //        user.setHomeDirectory("home");
 //        user.setRole("user");
 
-        System.out.println(props.getUploadDir());
+//        System.out.println(props.getUploadDir());
+        String baseDir = props.getUploadDir();
+
+        Path basePath = Paths.get(baseDir);
+
+        Path homeDir = Paths.get(baseDir, user.getUserName());
+        FilesystemAPI.createDir(user, basePath, user.getUserName());
+        user.setHomeDirectory(homeDir.toAbsolutePath().toString());
 
         return userService.createAccount(user);
     }
