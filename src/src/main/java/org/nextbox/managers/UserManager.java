937cfb778570a07dfffa64296298b14c9abfbc6f
Saurabh Sood <saurabhsood91@gmail.com>
2017-04-19 20:38:31 -0600

    add rudimentary properties

--- src/src/main/java/org/nextbox/managers/UserManager.java
+++ src/src/main/java/org/nextbox/managers/UserManager.java
@@ -1,5 +1,6 @@
 package org.nextbox.managers;
 
+import org.nextbox.beans.NextboxProperties;
 import org.nextbox.model.User;
 import org.nextbox.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,19 @@ public class UserManager {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private NextboxProperties props;
+
     public User getUserByUsername(String username) {
         return userService.getUserByUsername(username);
     }
 
     public boolean createAccount(User user) {
-        user.setHomeDirectory("home");
+//        user.setHomeDirectory("home");
 //        user.setRole("user");
+
+        System.out.println(props.getUploadDir());
+
         return userService.createAccount(user);
     }
 }
